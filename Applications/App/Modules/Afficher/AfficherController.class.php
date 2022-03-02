3a7f2261d7caff1ed1cec3c7c2e6454786b73878
dione24 <dioneniangaly@gmail.com>
2022-03-02 18:37:52 +0000

    Test

--- Applications/App/Modules/Afficher/AfficherController.class.php
+++ Applications/App/Modules/Afficher/AfficherController.class.php
@@ -7,12 +7,12 @@ class AfficherController extends \Library\BackController
     public function executeHome(\Library\HTTPRequest $request)
     {
         $this->page->addVar("titles", "Accueil"); // Titre de la page
-        $FicheDebiteur = $this->managers->getManagerOf('Afficher')->getFicheDebiteur();
+        $FicheDebiteur = $this->managers->getManagerOf('Afficher')->getFicheDebiteur(501);
         $this->page->addVar("FicheDebiteur", $FicheDebiteur); // Titre de la page
-        $SumPayementDebiSuvi = $this->managers->getManagerOf('Afficher')->SumPayementDebiSuvi();
+        $SumPayementDebiSuvi = $this->managers->getManagerOf('Afficher')->SumPayementDebiSuvi(501);
         $this->page->addVar("SumPayementDebiSuvi", $SumPayementDebiSuvi); // Titre de la page
 
-        $paiementDebiteur = $this->managers->getManagerOf('Afficher')->PayementDebiteur();
+        $paiementDebiteur = $this->managers->getManagerOf('Afficher')->PayementDebiteur(501);
         $this->page->addVar("paiementDebiteur", $paiementDebiteur); // Titre de la page
 
 
