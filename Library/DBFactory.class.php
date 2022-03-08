6102710ee99494a902c90b678dd395138ba701db
dione24 <dioneniangaly@gmail.com>
2022-03-08 13:40:45 +0000

    init

--- Library/DBFactory.class.php
+++ Library/DBFactory.class.php
@@ -6,13 +6,13 @@ class DBFactory
 {
 	public static function MySQLPDO()
 	{
-		$db = new \PDO('mysql:host=localhost;dbname=dash;charset=utf8', 'root', 'root');
+		$db = new \PDO('mysql:host=localhost;dbname=mcr;charset=utf8', 'root', 'root');
 		$db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
 
 		return $db;
 	}
 	public static function MySQLMySQLi()
 	{
-		return new \MySQLi('localhost', 'root', 'root', 'dash');
+		return new \MySQLi('localhost', 'root', 'root', 'mcr');
 	}
 }
