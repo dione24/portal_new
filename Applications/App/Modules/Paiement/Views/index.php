c154ff32504ba84da5594332d72141b031962965
dione24 <dioneniangaly@gmail.com>
2022-03-08 13:59:01 +0000

    check

--- Applications/App/Modules/Paiement/Views/index.php
+++ Applications/App/Modules/Paiement/Views/index.php
@@ -1,3 +1,61 @@
+<?php
+$apikey = '9740230376227497ddde1b0.56157923';
+$cpm_site_id = '9740230376227497ddde1b0.56157923';
+$cpm_version = 'V1';
+$cpm_language = 'fr';
+$cpm_currency = 'CFA';
+$cpm_page_action = 'PAYMENT';
+$cpm_payment_config = 'SINGLE';
+$cpSecure = "https://secure.cinetpay.com";
+$signatureUrl = "https://api.cinetpay.com/v1/?method=getSignatureByPost";
+$cpm_amount = '';
+$cpm_custom = ''; // This field exist soanything can be inserted in it;it will be send back after payment
+$cpm_designation = 'Payement'; //this field exist to identify the article being paid
+$cpm_trans_date = date("Y-m-d H:i:s");
+$cpm_trans_id = 'payement-' . (string)date("YmdHis"); //Transaction id that will be send to identify the transaction
+$return_url = "https://portal.malicreances-sa.com"; //The customer will be redirect on this page after successful payment
+$cancel_url = "https://portal.malicreances-sa.com"; //The customer will be redirect on this page if the payment get cancel
+$notify_url = ""; //This page must be a webhook (webservice).
+//it will be called weither or nor the payment is success or failed
+//you must only listen to this to update transactions status
+//Data that will be send in the form
+$getSignatureData = array(
+    'apikey' => $apikey,
+    'cpm_amount' => $cpm_amount,
+    'cpm_custom' => $cpm_custom,
+    'cpm_site_id' => $cpm_site_id,
+    'cpm_version' => $cpm_version,
+    'cpm_currency' => $cpm_currency,
+    'cpm_trans_id' => $cpm_trans_id,
+    'cpm_language' => $cpm_language,
+    'cpm_trans_date' => $cpm_trans_date,
+    'cpm_page_action' => $cpm_page_action,
+    'cpm_designation' => $cpm_designation,
+    'cpm_payment_config' => $cpm_payment_config
+);
+// use key 'http' even if you send the request to https://...
+$options = array(
+    'http' => array(
+        'method' => "POST",
+        'header' => "Content-Type: application/x-www-form-urlencoded\r\n",
+        'content' => http_build_query($getSignatureData)
+    )
+);
+// $context = stream_context_create($options);
+// $result = file_get_contents($signatureUrl, false, $context);
+// if ($result === false) {
+//     /* Handle error */
+//     \header($return_url);
+//     exit();
+// }
+// // var_dump($getSignatureData);
+// // echo("\n");
+// $signature = json_decode($result);
+// // var_dump($signature);
+?>
+
+
+
 <div class="checkout-tabs">
     <div class="row">
         <div class="col-xl-2 col-sm-3">
@@ -5,12 +63,7 @@
                 <a class="nav-link active" id="v-pills-shipping-tab" data-bs-toggle="pill" href="#v-pills-shipping"
                     role="tab" aria-controls="v-pills-shipping" aria-selected="true">
                     <i class="bx bx-money  d-block check-nav-icon mt-4 mb-2"></i>
-                    <p class="fw-bold mb-4">Mobile Money Payment</p>
-                </a>
-                <a class="nav-link" id="v-pills-payment-tab" data-bs-toggle="pill" href="#v-pills-payment" role="tab"
-                    aria-controls="v-pills-payment" aria-selected="false">
-                    <i class="bx bx-money d-block check-nav-icon mt-4 mb-2"></i>
-                    <p class="fw-bold mb-4">Card Payment</p>
+                    <p class="fw-bold mb-4">Paiement via CinetPay</p>
                 </a>
             </div>
         </div>
@@ -21,8 +74,8 @@
                         <div class="tab-pane fade show active" id="v-pills-shipping" role="tabpanel"
                             aria-labelledby="v-pills-shipping-tab">
                             <div>
-                                <h4 class="card-title">Mobile Money Payment</h4>
-                                <p class="card-title-desc">Remplissez toutes les informations ci-dessous</p>
+                                <h4 class="card-title">Cinet Pay</h4>
+                                <img src="/images/latest_inline.png" alt="Logo CinetPay">
                                 <form method="post" action="<?= $cpSecure; ?>">
                                     <div>
                                         <label for="billing-money" class="col-md-2 col-form-label">Montant</label>
@@ -61,62 +114,7 @@
                                 </form>
                             </div>
                         </div>
-                        <div class="tab-pane fade" id="v-pills-payment" role="tabpanel"
-                            aria-labelledby="v-pills-payment-tab">
-                            <div>
-                                <h4 class="card-title">Payment information</h4>
-                                <p class="card-title-desc">Fill all information below</p>
 
-                                <div class="form-group mt-4 mb-0">
-                                    <div class="form-check form-check-inline font-size-16">
-                                        <input class="form-check-input" type="radio" name="paymentoptionsRadio"
-                                            id="paymentoptionsRadio1" checked>
-                                        <label class="form-check-label font-size-13" for="paymentoptionsRadio1"><i
-                                                class="fab fa-cc-mastercard me-1 font-size-20 align-top"></i>
-                                            Credit / Debit Card</label>
-                                    </div>
-                                </div>
-                                <div>
-                                    <label for="billing-money" class="col-md-2 col-form-label">Montant</label>
-                                    <div class="col-md-5">
-                                        <input type="text" class="form-control" id="billing-phone"
-                                            placeholder="Entrer le montant ">
-                                    </div>
-                                </div></br>
-                                <div class="p-4 border">
-                                    <form>
-                                        <div class="form-group mb-0">
-                                            <label for="cardnumberInput">Card Number</label>
-                                            <input type="text" class="form-control" id="cardnumberInput"
-                                                placeholder="0000 0000 0000 0000">
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-lg-6">
-                                                <div class="form-group mt-4 mb-0">
-                                                    <label for="cardnameInput">Name on card</label>
-                                                    <input type="text" class="form-control" id="cardnameInput"
-                                                        placeholder="Name on Card">
-                                                </div>
-                                            </div>
-                                            <div class="col-lg-3">
-                                                <div class="form-group mt-4 mb-0">
-                                                    <label for="expirydateInput">Expiry date</label>
-                                                    <input type="text" class="form-control" id="expirydateInput"
-                                                        placeholder="MM/YY">
-                                                </div>
-                                            </div>
-                                            <div class="col-lg-3">
-                                                <div class="form-group mt-4 mb-0">
-                                                    <label for="cvvcodeInput">CVV Code</label>
-                                                    <input type="text" class="form-control" id="cvvcodeInput"
-                                                        placeholder="Enter CVV Code">
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
